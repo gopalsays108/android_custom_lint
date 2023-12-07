d2f4e03484f45b57e78ad34d823fd0138ea8c61d
Gopal <gopalsays108@gmail.com>
2023-12-07 16:07:06 +0530

    added example

--- app/src/main/java/gopal/androidcustomlint/MainActivity.kt
+++ app/src/main/java/gopal/androidcustomlint/MainActivity.kt
@@ -2,11 +2,29 @@ package gopal.androidcustomlint
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.annotation.UiThread
+import androidx.annotation.WorkerThread
 
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+
+    //To catch this error we can run Wrong Thread ( Interprocedural ) inspection
+    @UiThread
+    private fun UiThread(){
+        foo1()
+    }
+
+    private fun foo1(){
+        networkCall()
+    }
+
+    @WorkerThread
+    private fun networkCall(){
+        //Network
     }
 }
